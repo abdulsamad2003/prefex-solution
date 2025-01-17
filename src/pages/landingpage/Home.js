31b35cd07e837590b2480a71256e8fa6743fca37
user <samadsufiyan777888666@gmail.com>
2025-01-18 02:36:11 +0530

    improve performance

--- src/pages/landingpage/Home.js
+++ src/pages/landingpage/Home.js
@@ -9,9 +9,9 @@ const Home = () => {
         <h1 className="main-font">
           GLOBAL <span>edge</span>
         </h1>
-        <p className="para-font">
-          GLOBAL EDGE is a group of professional traders and financial advisors,
-          well versed with <br /> the alpha and omega of various assets in the
+        <p style={{paddingTop: '2vh'}} className="para-font">
+          GLOBAL EDGE is a group of professional traders and financial advisors </p>
+        <p style={{paddingBottom: '2vh'}} className="para-font"> well versed with the alpha and omega of various assets in the
           International Financial Market.
         </p>
         <Link to="/contact" className="hero-btn" aria-label="Contact Us">
