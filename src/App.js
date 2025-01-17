35fce64aca416253c76c4c573c6153ddec5c7f97
user <samadsufiyan777888666@gmail.com>
2025-01-18 01:57:38 +0530

    improve performance

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet';
 import './App.scss';
 import Nav from './componets/Nav';
 import Home from './pages/landingpage/Home';
@@ -6,6 +7,13 @@ import Home from './pages/landingpage/Home';
 function App() {
   return (
    <>
+   {/*for preloading fonts */}
+     <Helmet>
+     <link rel="preload" as="font" href="https://fonts.googleapis.com" crossorigin="anonymous"/>
+     <link rel="preload" as="font" href="https://fonts.gstatic.com" crossorigin="anonymous"/>
+     <link rel="preload" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
+    
+      </Helmet>
       <Nav/>
       <Home/>
    </>
