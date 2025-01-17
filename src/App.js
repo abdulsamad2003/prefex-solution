ab4700182b06d76b1435be48c14605328ee50de9
user <samadsufiyan777888666@gmail.com>
2025-01-18 02:53:43 +0530

    improve performance

--- src/App.js
+++ src/App.js
@@ -11,10 +11,7 @@ function App() {
      <Helmet>
      <link rel="preload" as="font" href="https://fonts.googleapis.com" crossorigin="anonymous"/>
      <link rel="preload" as="font" href="https://fonts.gstatic.com" crossorigin="anonymous"/>
-     <link rel="preload" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
-    
-       <link rel="preload" href="/assets/logo.webp" as="image" />
-    <link rel="preload" href="/assets/home.webp" as="image" />
+     <link rel="preload" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" />
       </Helmet>
       <Nav/>
       <Home/>
