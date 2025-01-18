a94b6693c709e524ae303d41f80fcba42805f17f
user <samadsufiyan777888666@gmail.com>
2025-01-18 12:23:05 +0530

    home page completed

--- src/pages/landingpage/Home.js
+++ src/pages/landingpage/Home.js
@@ -9,11 +9,13 @@ const Home = () => {
         <h1 className="main-font">
           GLOBAL <span>edge</span>
         </h1>
-        <p style={{paddingTop: '2vh'}} className="para-font">
-          GLOBAL EDGE is a group of professional traders and financial advisors </p>
-        <p style={{paddingBottom: '2vh'}} className="para-font"> well versed with the alpha and omega of various assets in the
-          International Financial Market.
-        </p>
+        <div className="para">
+            <p className="para-font">
+            GLOBAL EDGE is a group of professional traders and financial advisors </p>
+            <p className="para-font"> well versed with the alpha and omega of various assets in the
+            International Financial Market.
+            </p>
+        </div>
         <Link to="/contact" className="hero-btn" aria-label="Contact Us">
           Contact Us
         </Link>
