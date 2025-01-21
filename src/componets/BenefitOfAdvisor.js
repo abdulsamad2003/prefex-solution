95c0cbf1ac12731094fe044ab0e2dc3fcf55e5a1
user <samadsufiyan777888666@gmail.com>
2025-01-21 20:46:24 +0530

    edit benefit from advisor component

--- src/componets/BenefitOfAdvisor.js
+++ src/componets/BenefitOfAdvisor.js
@@ -20,6 +20,25 @@ const BenefitOfAdvisor = () => {
                 </p>
             </div>
         </div>
+
+        <div className="benefit-card">
+            <div className="image-container">
+                <img src={financialAdivisor} width={200} alt="" aria-label='' />
+            </div>
+            <div className="content">
+                <h1 className="main-font">
+                DO NOT be fooled by their flashy advertising and promises of higher returns!
+                </h1>
+                <p className="para-font">
+                 > What would be best is if YOU create the EA yourself. We won’t lie – it ain’t particularly easy and you may have to spend some time figuring it out, but hey, if it was easy, then everyone would do it right?
+                </p>
+                <p className="para-font">
+                 > We highly encourage you to learn and understand all aspects of whatever Expert Advisor that you plan to use.
+                </p>
+            </div>
+        </div>
+
+        
     </section>
   )
 }
